4a158dcc2d1a8efab87cf8efb1113d27f24ee0ba
dineyw23 <diney.12323@gmail.com>
2017-04-09 02:17:18 -0700

    Removing quotes around env variables

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var twit = require('twit');
 var emoji = require('node-emoji');
 
 var Twitter = new twit(
-   {consumer_key: 'process.env.CONSUMER_KEY',
-   consumer_secret: 'process.env.CONSUMER_SECRET',
-   access_token: 'process.env.ACCESS_TOKEN',
-   access_token_secret: 'process.env.ACCESS_TOKEN_SECRET' 
+   {
+   consumer_key: process.env.CONSUMER_KEY,
+   consumer_secret: process.env.CONSUMER_SECRET,
+   access_token: process.env.ACCESS_TOKEN,
+   access_token_secret: process.env.ACCESS_TOKEN_SECRET 
 });
 
 function tweet() {
